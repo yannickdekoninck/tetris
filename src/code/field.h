5883014303cd690e0758613a7d153dd22e853723
Yannick De Koninck <yannick.dekoninck@gmail.com>
2021-03-08 21:36:47 +0100

    init and drawing the field

--- src/code/field.h
+++ src/code/field.h
@@ -12,9 +12,15 @@ typedef struct Fields
     short *field;
     short rows;
     short columns;
+    short pitch_x;
+    short pitch_y;
 } Field;
 
-void initialize_field(Field *field, int rows, int columns);
+short get_field_value(Field *field, short x, short y);
+
+void set_field_value(Field *field, short x, short y, short value);
+
+void initialize_field(Field *field, short rows, short columns, short pitch_x, short pitch_y);
 
 void draw_field(Field *field, FieldItem *lookup_table, int center_x, int center_y);
 
