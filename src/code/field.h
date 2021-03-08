f7451a6e9cef2f41abd42f5051a96e868e786618
Yannick De Koninck <yannick.dekoninck@gmail.com>
2021-03-08 20:58:31 +0100

    Basic interfaces

---
+++ src/code/field.h
@@ -0,0 +1,21 @@
+#ifndef FIELD_H
+#define FIELDH
+#include "graphics.h"
+
+typedef struct FieldItems
+{
+    Sprite *field_sprite;
+} FieldItem;
+
+typedef struct Fields
+{
+    short **field;
+    short rows;
+    short columns;
+} Field;
+
+void initialize_field(Field *field, int rows, int columns);
+
+void draw_field(Field *field, FieldItems *lookup_table, int center_x, int center_y);
+
+#endif
