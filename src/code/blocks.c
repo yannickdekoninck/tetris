ba46352fafcdd88b3b116b112a5f5afae0cd12d6
Yannick De Koninck <yannick.dekoninck@gmail.com>
2021-03-11 19:32:00 +0100

    Adding triangle

--- src/code/blocks.c
+++ src/code/blocks.c
@@ -5,43 +5,84 @@ Block *initialize_block_list()
 {
 
     Block *block_list = malloc(sizeof(Block) * 5);
-
+    Coord **coords;
     // square
-    Coord **coords0 = malloc(sizeof(Coord *) * 1);
-    coords0[0] = malloc(sizeof(Coord) * 4);
-    coords0[0][0].x = 0;
-    coords0[0][0].y = 0;
-    coords0[0][1].x = 1;
-    coords0[0][1].y = 0;
-    coords0[0][2].x = 0;
-    coords0[0][2].y = 1;
-    coords0[0][3].x = 1;
-    coords0[0][3].y = 1;
-    block_list[0].orientations = coords0;
+    coords = malloc(sizeof(Coord *) * 1);
+    coords[0] = malloc(sizeof(Coord) * 4);
+    coords[0][0].x = 0;
+    coords[0][0].y = 0;
+    coords[0][1].x = 1;
+    coords[0][1].y = 0;
+    coords[0][2].x = 0;
+    coords[0][2].y = 1;
+    coords[0][3].x = 1;
+    coords[0][3].y = 1;
+    block_list[0].orientations = coords;
     block_list[0].orientations_count = 1;
 
     // line
-    Coord **coords1 = malloc(sizeof(Coord *) * 2);
-    coords1[0] = malloc(sizeof(Coord) * 4);
-    coords1[0][0].x = -1;
-    coords1[0][0].y = 0;
-    coords1[0][1].x = 0;
-    coords1[0][1].y = 0;
-    coords1[0][2].x = 1;
-    coords1[0][2].y = 0;
-    coords1[0][3].x = 2;
-    coords1[0][3].y = 0;
-    coords1[1] = malloc(sizeof(Coord) * 4);
-    coords1[1][0].x = 0;
-    coords1[1][0].y = -1;
-    coords1[1][1].x = 0;
-    coords1[1][1].y = 0;
-    coords1[1][2].x = 0;
-    coords1[1][2].y = 1;
-    coords1[1][3].x = 0;
-    coords1[1][3].y = 2;
-    block_list[1].orientations = coords1;
+    coords = malloc(sizeof(Coord *) * 2);
+    coords[0] = malloc(sizeof(Coord) * 4);
+    coords[0][0].x = -1;
+    coords[0][0].y = 0;
+    coords[0][1].x = 0;
+    coords[0][1].y = 0;
+    coords[0][2].x = 1;
+    coords[0][2].y = 0;
+    coords[0][3].x = 2;
+    coords[0][3].y = 0;
+    coords[1] = malloc(sizeof(Coord) * 4);
+    coords[1][0].x = 0;
+    coords[1][0].y = -1;
+    coords[1][1].x = 0;
+    coords[1][1].y = 0;
+    coords[1][2].x = 0;
+    coords[1][2].y = 1;
+    coords[1][3].x = 0;
+    coords[1][3].y = 2;
+    block_list[1].orientations = coords;
     block_list[1].orientations_count = 2;
+
+    // Triangle
+    coords = malloc(sizeof(Coord *) * 4);
+    coords[0] = malloc(sizeof(Coord) * 4);
+    coords[0][0].x = -1;
+    coords[0][0].y = 0;
+    coords[0][1].x = 0;
+    coords[0][1].y = 0;
+    coords[0][2].x = 1;
+    coords[0][2].y = 0;
+    coords[0][3].x = 0;
+    coords[0][3].y = 1;
+    coords[1] = malloc(sizeof(Coord) * 4);
+    coords[1][0].x = 0;
+    coords[1][0].y = -1;
+    coords[1][1].x = 0;
+    coords[1][1].y = 0;
+    coords[1][2].x = 0;
+    coords[1][2].y = 1;
+    coords[1][3].x = 1;
+    coords[1][3].y = 0;
+    coords[2] = malloc(sizeof(Coord) * 4);
+    coords[2][0].x = -1;
+    coords[2][0].y = 0;
+    coords[2][1].x = 0;
+    coords[2][1].y = 0;
+    coords[2][2].x = 1;
+    coords[2][2].y = 0;
+    coords[2][3].x = 0;
+    coords[2][3].y = -1;
+    coords[3] = malloc(sizeof(Coord) * 4);
+    coords[3][0].x = 0;
+    coords[3][0].y = -1;
+    coords[3][1].x = 0;
+    coords[3][1].y = 0;
+    coords[3][2].x = 0;
+    coords[3][2].y = 1;
+    coords[3][3].x = -1;
+    coords[3][3].y = 0;
+    block_list[2].orientations = coords;
+    block_list[2].orientations_count = 4;
 
     return block_list;
 }
